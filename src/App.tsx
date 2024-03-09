8ffe3af0186161171f32ab43261bc29087a0bd1a
AluviiInc <scott@aluvii.com>
2024-03-08 22:48:07 -0700

    1st Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Scott's React App</div>
+    </>
+  );
+}
+
+
+
+export default App;
+
+
+
