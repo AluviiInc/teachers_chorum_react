daf07cd29e3fe1cb8af065a2f0b125c1943922ed
AluviiInc <scott@aluvii.com>
2024-03-11 15:31:07 -0600

    side nav done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
-
 import "./NavbarStyles.css";
 import ldsChurchImage from "../assets/ldschurch.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menuClicked, setMenuClicked] = useState(false)
+  const handleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  }
   const navItems = [
     { text: "Calendar", link: "index.html", isActive: true },
     { text: "Pictures", link: "index.html", isActive: false },
-    
   ];
 
   return (
@@ -22,6 +27,13 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div id="mobile">
+          {menuClicked ? (
+            <i><CloseIcon onClick={handleMenuClick} /></i>
+          ) : (
+           <i><MenuIcon onClick={handleMenuClick} /></i>
+          )}
+        </div>
       </nav>
     </>
   );
